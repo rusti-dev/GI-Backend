5709d26511e1483136a332e2af9e51251d7f198c
Luis Gabriel Janco <janco7249@gmail.com>
2024-02-13 00:15:57 -0400

    feat: add data source config

---
+++ src/config/app.config.ts
@@ -0,0 +1,28 @@
+export const EnvConfig = () => ({
+  APP_NAME: process.env.APP_NAME || 'NestJS-Template',
+  APP_PROD: process.env.APP_PROD || false,
+  APP_VERSION: process.env.APP_VERSION || '0.0.1',
+  DOCKER_IMAGE: process.env.DOCKER_IMAGE || '',
+  PORT: process.env.PORT || 3000,
+
+  APP_URL: process.env.APP_URL || 'http://localhost:3000',
+  FRONTEND_URL: process.env.FRONTEND_URL || 'http://localhost:5173',
+
+  DB_CONNECTION: process.env.DB_CONNECTION || 'mysql',
+  DB_HOST: process.env.DB_HOST || 'localhost',
+  DB_PORT: process.env.DB_PORT || 5432,
+  DB_DATABASE: process.env.DB_DATABASE || 'mysql',
+  DB_USERNAME: process.env.DB_USERNAME || 'mysql',
+  DB_PASSWORD: process.env.DB_PASSWORD || 'mysql',
+
+  HASH_SALT: process.env.HASH_SALT || 10,
+  JWT_AUTH: process.env.JWT_AUTH || 'secret',
+  JWT_RECOVERY: process.env.JWT_RECOVERY || 'secret',
+
+  GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+  GOOGLE_SECRET: process.env.GOOGLE_SECRET,
+
+  MAILER_SERVICE: process.env.MAILER_SERVICE || 'gmail',
+  MAILER_EMAIL: process.env.MAILER_EMAIL,
+  MAILER_SECRET_KEY: process.env.MAILER_SECRET_KEY,
+});
---
+++ src/config/data.source.ts
@@ -0,0 +1,30 @@
+import { DataSource, DataSourceOptions } from 'typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+
+ConfigModule.forRoot({ envFilePath: '.env' });
+const configService = new ConfigService();
+
+export const DataSourceConfig: DataSourceOptions = {
+  type: 'mysql',
+  host: configService.get('DB_HOST'),
+  port: configService.get('DB_PORT'),
+  username: configService.get('DB_USERNAME'),
+  password: configService.get('DB_PASSWORD'),
+  database: configService.get('DB_DATABASE'),
+  entities: [__dirname + '/../**/**/*.entity{.ts,.js}'],
+  // migrations: [__dirname + '/../migrations/*{.ts,.js}'],
+  // synchronize: configService.get('APP_PROD') === 'true' ? true : false,
+  synchronize: true,
+  // logging: false,
+  // extra: {
+  //   ssl:
+  //     configService.get('APP_PROD') === 'true'
+  //       ? { rejectUnauthorized: false }
+  //       : null,
+  //   timezone: 'America/La_Paz',
+  // },
+  namingStrategy: new SnakeNamingStrategy(),
+};
+
+export const AppDS = new DataSource(DataSourceConfig);
