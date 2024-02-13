b2badc9173dc427bd934fc57ddca1f47ade06005
Luis Gabriel Janco <janco7249@gmail.com>
2024-02-13 00:16:56 -0400

    feat: add example controller

---
+++ src/modules/example/entities/example.entity.ts
@@ -0,0 +1,10 @@
+import { Column, Entity } from 'typeorm';
+
+import { BaseEntity } from '../../../common/entities/base.entity';
+import { IExample } from '../interfaces/example.interface';
+
+// @Entity({ name: 'example' })
+export class ExampleEntity extends BaseEntity implements IExample {
+  @Column()
+  name: string;
+}
---
+++ src/modules/example/services/example.service.ts
@@ -0,0 +1,68 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { CreateExampleDto } from '../dto/create-example.dto';
+import { UpdateExampleDto } from '../dto/update-example.dto';
+import { ExampleEntity } from '../entities/example.entity';
+import { QueryDto } from '../../../common/dto/query.dto';
+import { handlerError } from '../../../common/utils/handlerError.utils';
+import { ResponseMessage } from '../../../common/interfaces/responseMessage.interface';
+
+@Injectable()
+export class ExampleService {
+  private readonly logger = new Logger('ExampleService');
+
+  constructor(
+    @InjectRepository(ExampleEntity)
+    private readonly exampleRepository: Repository<ExampleEntity>,
+  ) { }
+
+  public async create(
+    createEstadioDto: CreateExampleDto,
+  ): Promise<ResponseMessage> {
+    try {
+      return null;
+    } catch (error) {
+      handlerError(error, this.logger);
+    }
+  }
+
+  public async findAll(queryDto: QueryDto): Promise<ResponseMessage> {
+    try {
+      return null;
+    } catch (error) {
+      handlerError(error, this.logger);
+    }
+  }
+
+  public async findOne(id: string): Promise<ResponseMessage> {
+    try {
+      return null;
+    } catch (error) {
+      handlerError(error, this.logger);
+    }
+  }
+
+  public async update(
+    id: string,
+    updateExampleDto: UpdateExampleDto,
+  ): Promise<ResponseMessage> {
+    try {
+      return null;
+    } catch (error) {
+      handlerError(error, this.logger);
+    }
+  }
+
+  public async remove(id: string): Promise<ResponseMessage> {
+    try {
+      return {
+        message: 'Example deleted successfully',
+        statusCode: 200,
+      };
+    } catch (error) {
+      handlerError(error, this.logger);
+    }
+  }
+}
