54ef73238770cb39c76cbb7acce5a3229e1170b1
Luis Gabriel Janco <janco7249@gmail.com>
2024-03-17 17:37:45 -0400

    chore(root): ignore file yarn.lock on .gitignore

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,23 +3,24 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { DataSourceConfig } from './config/data.source';
-import { UsersModule } from './modules/users/users.module';
-import { AuthModule } from './modules/auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 import { ProvidersModule } from './providers/providers.module';
 import { CommonModule } from './common/common.module';
 import { SeederModule } from './seeder/seeder.module';
-import { TiendaModule } from './modules/tienda/tienda.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
     TypeOrmModule.forRoot({ ...DataSourceConfig }),
+
     ProvidersModule,
     CommonModule,
+
     UsersModule,
     AuthModule,
+
     SeederModule,
-    TiendaModule
   ]
 })
 export class AppModule { }
