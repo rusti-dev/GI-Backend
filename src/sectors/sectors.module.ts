bf5dc68f18c029d45ef3133cb84e2dbee434e9c8
Ilse Romero <ilseromero35@gmail.com>
2025-04-13 15:38:17 -0400

    fix(users):  add users module

--- src/sectors/sectors.module.ts
+++ src/sectors/sectors.module.ts
@@ -3,12 +3,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { SectorsService } from './sectors.service';
 import { SectorEntity } from './entities/sector.entity';
 import { SectorsController } from './sectors.controller';
+import { UsersModule } from '@/users/users.module';
 
 
 
 @Module({
     imports: [
         TypeOrmModule.forFeature([ SectorEntity ]),
+        UsersModule,
     ],
     controllers: [ SectorsController ],
     providers: [ SectorsService ],
