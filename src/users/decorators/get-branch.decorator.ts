b0114a7271e388140a6e2f1818718c7b0c28ce74
Ilse Romero <ilseromero35@gmail.com>
2025-04-13 06:58:56 -0400

    refactor: remove branch decorator

--- src/users/decorators/get-branch.decorator.ts
+++
@@ -1,9 +0,0 @@
-import { createParamDecorator, ExecutionContext} from '@nestjs/common';
-
-export const GetBranch = createParamDecorator(
-  (data: string, ctx: ExecutionContext) => {
-    const req = ctx.switchToHttp().getRequest();
-    const branch = req.branchId;
-    return branch ? branch : branch[data];
-  }
-);
