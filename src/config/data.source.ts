1c88b73938a24386b235def0f2173d0684944a9e
Alex Ticlla <aticlla.09@gmail.com>
2025-05-04 11:35:35 -0400

    data.source
    comentario ssl: configService

--- src/config/data.source.ts
+++ src/config/data.source.ts
@@ -17,9 +17,10 @@ export const DataSourceConfig: DataSourceOptions = {
   synchronize: true,
   namingStrategy: new SnakeNamingStrategy(),
   logging: false,
- /* extra: {
+  // esto no debe estar comentado cuando se ejecuta el backend
+  extra: {
     ssl: configService.get('APP_PROD') === 'true' ? { rejectUnauthorized: false } : { rejectUnauthorized: false }
-  }*/
+  }
 };
 
 export const AppDS = new DataSource(DataSourceConfig);
